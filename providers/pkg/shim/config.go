b4c4504d0e370f93459466b5789cc01b8ae30c31
Xiaoxuan Wang <wangxiaoxuan119@gmail.com>
2026-09-08 02:42:13 +0000

    lint
    Signed-off-by: Xiaoxuan Wang <wangxiaoxuan119@gmail.com>

--- providers/pkg/shim/config.go
+++ providers/pkg/shim/config.go
@@ -213,7 +213,8 @@ func IsAPIResourceInstalled(
 }
 
 func hasAPIResource(discoveryClient discovery.DiscoveryInterface, group, version, resource string) bool {
-resources, err := discoveryClient.ServerResourcesForGroupVersion(schema.GroupVersion{Group: group, Version: version}.String())
+	groupVersion := schema.GroupVersion{Group: group, Version: version}.String()
+	resources, err := discoveryClient.ServerResourcesForGroupVersion(groupVersion)
 	if err != nil {
 		return false
 	}
